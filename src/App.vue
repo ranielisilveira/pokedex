87236fa12759d8bf4751f90d0e987ae34072a60f
Ranieli <ranielisilveira@hotmail.com>
2022-07-13 17:25:23 -0300

    Create component Evolution

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,110 @@
 <template>
   <v-app>
     <v-container>
-      <v-card>
-        <v-container>
-          <v-text-field v-model="search" label="Pesquisar" placeholder="Pesquisar Pokemon" solo></v-text-field>
-          <v-row>
-            <v-col cols="2" v-for="pokemon in filteredPokemons" :key="pokemon.name">
-              <v-card>
-                <v-container>
-                  <v-row class="mx-0 d-flex justify-center">
-                    <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonId(
-                      pokemon
-                    )}.png`" :alt="pokemon.name" width="80%" />
-                  </v-row>
-                  <h2 class="text-center">{{ getPokemonName(pokemon) }}</h2>
-                </v-container>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
+      <v-container>
+        <v-row>
+          <v-container>
+            <v-img
+              :src="require('../src/assets/pokedex.png')"
+              class="my-3"
+              contain
+              height="200"
+            />
+            <h1 class="text-center white--text mb-2" style="font-size: 5rem">
+              Pokedex
+            </h1>
+          </v-container>
+        </v-row>
+
+        <v-text-field
+          v-model="search"
+          label="Pesquisar"
+          placeholder="Charmander"
+          solo
+        ></v-text-field>
+
+        <v-row>
+          <v-col
+            cols="6"
+            md="2"
+            v-for="pokemon in filtered_pokemons"
+            :key="pokemon.name"
+          >
+            <PokemonCard :pokemon="pokemon" @clicked="show_pokemon" />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-container>
+
+    <PokemonInfoDialog
+      :show.sync="show_dialog"
+      :selected_pokemon="selected_pokemon"
+    />
   </v-app>
 </template>
 
 <script>
 import axios from "axios";
-
+import PokemonCard from "./components/PokemonCard.vue";
+import PokemonInfoDialog from "./components/PokemonInfoDialog.vue";
 export default {
   name: "App",
-
-  components: {},
-
+  components: {
+    PokemonCard,
+    PokemonInfoDialog,
+  },
   data() {
     return {
       pokemons: [],
-      search: ""
+      search: "",
+      show_dialog: false,
+      selected_pokemon: null,
     };
   },
-
   mounted() {
     axios
-      .get("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .get("https://pokeapi.co/api/v2/pokemon?limit=493")
       .then((response) => {
         this.pokemons = response.data.results;
       });
   },
-
   methods: {
-    getPokemonId(pokemon) {
-      return Number(pokemon.url.split("/")[6]);
+    show_pokemon(id) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+        this.selected_pokemon = response.data;
+        this.show_dialog = !this.show_dialog;
+      });
     },
-
-    getPokemonName(pokemon) {
-      return pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    get_move_level(move) {
+      for (let version of move.version_group_details) {
+        if (
+          version.version_group.name == "sword-shield" &&
+          version.move_learn_method.name == "level-up"
+        ) {
+          return version.level_learned_at;
+        }
+      }
+      return 0;
     },
   },
-
   computed: {
-    filteredPokemons() {
-      return this.pokemons.filter((pokemon) => {
-        return pokemon.name.toLowerCase().includes(this.search.toLowerCase());
+    filtered_pokemons() {
+      return this.pokemons.filter((item) => {
+        return item.name.includes(this.search);
       });
-    }
+    },
   },
 };
 </script>
 
 <style>
 #app {
-  background: linear-gradient(to bottom right,
+  background: linear-gradient(
+      to bottom right,
       rgba(10, 10, 10, 1),
-      rgba(12, 39, 63, 1)) no-repeat center center fixed !important;
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
